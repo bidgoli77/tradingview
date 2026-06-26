91dcfbc492c3b44d9949b2e1c9f69eb240b9870a
bidgoli77 <mehdieslami77@gmail.com>
2026-06-26 12:43:20 -0400

    main

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI, Request
 
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "TSX AI Webhook is running"}
+
+@app.post("/webhook")
+async def tradingview_webhook(request: Request):
+    data = await request.json()
+    print("Webhook received:", data)
+
+    return {
+        "status": "received",
+        "data": data
+    }
